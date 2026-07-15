f36a6897867bfea538cc6c73d67db2a05f327547
rakhat <yskak.rakhat@gmail.com>
2026-07-15 08:38:34 +0500

    15 07 26
    a

--- cpp/src/sliding-window/l424.cpp
+++ cpp/src/sliding-window/l424.cpp
@@ -9,12 +9,18 @@ using namespace std;
 class Solution {
  public:
   int characterReplacement(string s, int k) {
-    array<int, 26> win;
-    int res = 0, l = 0, r = 0, temp = k;
+    array<int, 26> win{};
+    int res = 0, l = 0, r = 0;
+    int max_freq = 0;
     while (r < s.size()) {
-      if (s[l] == s[r]) {
-        r++;
+      win[s[r] - 'A']++;
+      max_freq = max(max_freq, win[s[r] - 'A']);
+      if (r - l + 1 > max_freq + k) {
+        win[s[l] - 'A']--;
+        l++;
       }
+      res = max(res, r - l + 1);
+      r++;
     }
     return res;
   }
