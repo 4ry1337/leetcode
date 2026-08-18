496c17dc70cfa9abd311f033c8c82e61a1f938c1
Rakhat Yskak <yskak.rakhat@gmail.com>
2026-08-18 15:29:50 +0500

    18 08 26

--- cpp/src/backtracking/l17.cpp
+++ cpp/src/backtracking/l17.cpp
@@ -1,8 +1,33 @@
 #include <bits/stdc++.h>
 
+using namespace std;
+
 /* https://leetcode.com/problems/letter-combinations-of-a-phone-number/ */
 
 class Solution {
- unordered_map<char, vector<char>> mp =
-     {{'2', {''}}} public : vector<string> letterCombinations(string digits) {}
+ private:
+  vector<string> res;
+  unordered_map<char, vector<char>> mp{
+      {'2', {'a', 'b', 'c'}}, {'3', {'d', 'e', 'f'}},
+      {'4', {'g', 'h', 'i'}}, {'5', {'j', 'k', 'l'}},
+      {'6', {'m', 'n', 'o'}}, {'7', {'p', 'q', 'r', 's'}},
+      {'8', {'t', 'u', 'v'}}, {'9', {'w', 'x', 'y', 'z'}},
+  };
+  void backtrack(string& digits, int idx, string& curr) {
+    if (digits.size() == idx) {
+      res.push_back(curr);
+    }
+    for (char& c : mp[digits[idx]]) {
+      curr += c;
+      backtrack(digits, idx + 1, curr);
+      curr.pop_back();
+    }
+  }
+
+ public:
+  vector<string> letterCombinations(string digits) {
+    string curr = "";
+    backtrack(digits, 0, curr);
+    return res;
+  }
 };
